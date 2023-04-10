598d699d545182612da0fe49173207bed1bd3e6e
Andy Wardley <abw@wardley.org>
2023-04-10 10:57:23 +0100

    Fixed date format

--- vite.defs.js
+++ vite.defs.js
@@ -1,7 +1,7 @@
 import process from 'node:process'
 
 const PACKAGE_VERSION = JSON.stringify(process.env.npm_package_version)
-const BUILD_DATE      = JSON.stringify(new Date().toLocaleDateString())
+const BUILD_DATE      = JSON.stringify(new Date().toISOString().split('T')[0])
 
 export default {
   PACKAGE_VERSION,
