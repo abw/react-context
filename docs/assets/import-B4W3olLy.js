2c6ddb474d1363513c4e78ba138a6a8f8f0ba5d0
Andy Wardley <abw@wardley.org>
2024-09-22 15:19:22 +0100

    Docs build

---
+++ docs/assets/import-B4W3olLy.js
@@ -0,0 +1,2 @@
+const t=`import { Context, Generator } from '@abw/react-context'
+`;export{t as default};
---
+++ docs/assets/install-ADh0fdCf.js
@@ -0,0 +1,9 @@
+const n=`# using npm
+$ npm add @abw/react-context
+
+# using yarn
+$ yarn add @abw/react-context
+
+# using pnpm
+$ pnpm add @abw/react-context
+`;export{n as default};
