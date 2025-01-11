96868c37cc1967e7050641f85a07858bba87f76a
Andy Wardley <abw@wardley.org>
2025-01-11 12:54:54 +0000

    Updated typescript examples

--- web/lib/Typescript/Controls.tsx
+++ web/lib/Typescript/Controls.tsx
@@ -1,12 +1,11 @@
 import Counter from './Counter'
-import { CounterRenderProps } from './Types'
 
-const Controls = ({ inc, dec }: CounterRenderProps) =>
-  <div className="flex gap-2 brand">
-    <button onClick={() => dec(10)}>-10</button>
-    <button onClick={() => dec()}>-1</button>
-    <button onClick={() => inc()}>+1</button>
-    <button onClick={() => inc(10)}>+10</button>
-  </div>
-
-export default Counter.Consumer(Controls)
+export default Counter.Consumer(
+  ({ inc, dec }) =>
+    <div className="flex gap-2 brand">
+      <button onClick={() => dec(10)}>-10</button>
+      <button onClick={() => dec()}>-1</button>
+      <button onClick={() => inc()}>+1</button>
+      <button onClick={() => inc(10)}>+10</button>
+    </div>
+)
