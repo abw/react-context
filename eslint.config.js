340c33eab83444ebd458722b70ea87276abdcc4d
Andy Wardley <abw@wardley.org>
2025-01-18 16:53:38 +0000

    Updated types and various config files

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['{lib,test}/**/*.{ts,tsx}'],
+    files: ['{lib,test}/**/*.{ts,tsx}', 'test/setup.ts'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
