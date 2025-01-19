1069ca5af6174ca5b3d3c2b31a77c784111f30af
Andy Wardley <abw@wardley.org>
2025-01-19 11:47:34 +0000

    OK, we don't need duplicate web lib examples

--- web/lib/version2/Counter2/Count.jsx
+++
@@ -1,9 +0,0 @@
-import React   from 'react'
-import Counter from './Counter.js'
-
-const Count = ({count}) =>
-  <p>
-    The current count is {count}
-  </p>
-
-export default Counter.Consumer(Count)
