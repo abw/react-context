d93f3227f7ef5c63939c82528be90a8800800df2
Andy Wardley <abw@wardley.org>
2025-01-20 18:44:20 +0000

    Added caveat about moving defaultProps outside context class

---
+++ web/snippets/typescript/AmplifierWorkaround5.tsx
@@ -0,0 +1,16 @@
+import { Context, WithRequiredFrom } from '@abw/react-context'
+
+/* ...as before... */
+
+class Amplifier extends Context<
+  AmplifierProps,
+  AmplifierState
+> {
+  /* ...as before... */
+
+  getRenderProps() {
+    return {
+      ...this.config,
+      ...this.state
+  }
+}
